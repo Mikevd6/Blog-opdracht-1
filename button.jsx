f930386156c8966fdc8be6418f87e3ae2bb9d3dc
Mikevd6 <dagtakenlijst@gmail.com>
2024-12-15 15:34:44 -0300

    Create button.jsx

---
+++ button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Button({ text, onClick, type = 'button' }) {
+    return (
+        <button type={type} onClick={onClick}>
+            {text}
+        </button>
+    );
+}
